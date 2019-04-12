47dc0a448d4be1e65adff71936f763894ad186f8
猫小毛 <moskylit@gmail.com>
2019-04-12 20:41:55 +0800

    v2.0 feature-getdata completed !

--- WFACatPro/version.py
+++ WFACatPro/version.py
@@ -12,7 +12,7 @@ def print_version_info():
     print('Title: ' + 'WFACat: weibo friends deep analysis')
     print('Description: ' + 'Python friends\' network analysis for every one.')
     print('URL: ' + 'https://github.com/Marlous/WFACat')
-    print('Version: ' + 'v1.0')
+    print('Version: ' + 'v2.0')
     print('Author: ' + 'Marlous')
     print('Author E-mail: ' + 'Goonecat@foxmail.com')
     print('License: ' + 'MIT License')
